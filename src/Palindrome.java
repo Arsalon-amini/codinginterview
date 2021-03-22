f9e0be648a04cf5e854133a10f505843681c8c84
arsalon <arsalonamini2020@gmail.com>
2021-03-22 14:11:16 +0800

    isPalindrome - recusive solution in O(n) time, O(n) space and stringBuilder solution in O(n) time, O(1) space

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -14,4 +14,25 @@ public class Palindrome {
         }
         return true;
     }
+
+    //O(n) time
+    //O(1) space - using a stringBuilder mutable string
+    public static boolean isPalindrome1(String str){
+        StringBuilder reversed = new StringBuilder();
+        for(var i = str.length() -1; i >=0; i--){
+            reversed.append(str.charAt(i));
+        }
+        return str.contentEquals(reversed);
+    }
+
+    //O(n) time
+    // //O(n) space - using recursive stack
+    public static boolean isPalindrome2(String str){
+        return isPalindrome2(str, 0);
+    }
+    public static boolean isPalindrome2(String str, int i){
+        int j = str.length() - 1 - i;
+        return i >= j ? true : str.charAt(i) == str.charAt(j) && isPalindrome2(str, i+1);
+    }
+
 }
