6f480a131141214335f3e802b719541555757d3d
arsalon <arsalonamini2020@gmail.com>
2021-04-12 18:16:33 +0800

    ArrayOfProducts algorithm in O(n) time and O(n) space using only two linear traversals and one output array by combing steps two and three of the previous algorithm.

--- src/ArrayOfProducts.java
+++ src/ArrayOfProducts.java
@@ -40,4 +40,24 @@ public class ArrayOfProducts {
 
         return products;
     }
+
+    //O(n) time
+    //O(n) space
+    public static int [] arrayOfProductsVersion3(int [] array){
+        int [] products = new int[array.length];
+
+        int leftRunningProduct = 1;
+        for(int i =0; i < array.length; i++){
+            products[i] = leftRunningProduct;
+            leftRunningProduct *= array[i];
+        }
+
+        int rightRunningProduct = 1;
+        for(int i = array.length - 1; i >= 0; i--){
+            products[i] *= rightRunningProduct;
+            rightRunningProduct *= array[i];
+        }
+
+        return products;
+    }
 }
