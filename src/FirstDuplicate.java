3778f7fe7d38f85e4e28dd8855c0b24c783c7e5e
arsalon <arsalonamini2020@gmail.com>
2021-04-13 13:18:44 +0800

    Find First Duplicate Value algorithm - brute force approach, using a loop to iterate all elements, at each element initiating a nested traversal, finding any second occurance and updating the minimumSecondOccuranceIndex - uses O(n^2) time and O(1) space

--- src/FirstDuplicate.java
+++ src/FirstDuplicate.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class FirstDuplicate {
+    //O(n) time - single linear traversal
+    //O(n) space - storing values in set
     public static int findFirstDuplicate(int [] array){
         HashSet<Integer> seen = new HashSet<Integer>();
 
@@ -10,5 +12,22 @@ public class FirstDuplicate {
             seen.add(value);
         }
         return -1;
+    }
+
+    //O(n^2) time
+    //O(1) space
+    public static int returnFirstDuplicateValue(int [] array){
+        int secondMinimumIndex = array.length;
+
+        for(int i = 0; i < array.length; i++){
+            int value = array[i];
+            for(int j = i + 1; j < array.length; j++){
+                int valueToCompare = array[j];
+                if(value == valueToCompare) secondMinimumIndex = Math.min(secondMinimumIndex, j);
+            }
+        }
+        if(secondMinimumIndex == array.length) return -1;
+        int firstDuplicate = array[secondMinimumIndex];
+        return firstDuplicate;
     }
 }
