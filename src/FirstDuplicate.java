9c768787df59987556dec1dbb48c74f5dad16ec7
arsalon <arsalonamini2020@gmail.com>
2021-04-14 12:15:20 +0800

    return First Duplicate algorithm - using O(1) space and O(n) time, this commit contains the optimal solution. Every value is between 1-n, therefore, we can map every value to a valid index, if it's the first time seeing the value, mark it negative, if it's the second, chck if it's negative and if it is, we've seen it before so return it.

--- src/FirstDuplicate.java
+++ src/FirstDuplicate.java
@@ -2,18 +2,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class FirstDuplicate {
-    //O(n) time - single linear traversal
-    //O(n) space - storing values in set
-    public static int findFirstDuplicate(int [] array){
-        HashSet<Integer> seen = new HashSet<Integer>();
-
-        for(int value: array){
-            if(seen.contains(value)) return value;
-            seen.add(value);
-        }
-        return -1;
-    }
-
+    //#1
+    //Brute Force
     //O(n^2) time
     //O(1) space
     public static int returnFirstDuplicateValue(int [] array){
@@ -29,5 +19,32 @@ public class FirstDuplicate {
         if(secondMinimumIndex == array.length) return -1;
         int firstDuplicate = array[secondMinimumIndex];
         return firstDuplicate;
+    }
+
+    //#2
+    //Using a Set
+    //O(n) time - single linear traversal
+    //O(n) space - storing values in set
+    public static int findFirstDuplicate(int [] array){
+        HashSet<Integer> seen = new HashSet<Integer>();
+
+        for(int value: array){
+            if(seen.contains(value)) return value;
+            seen.add(value);
+        }
+        return -1;
+    }
+
+    //#3
+    //Using no external data structure - duplicate numbers map to the same index (5 - 1 = 4, array[4] < 0 ? )
+    //O(n) time
+    //O(1) space
+    public static int returnFirstDuplicateLeastSpace(int [] array){
+        for(int value: array){
+            int absValue = Math.abs(value);
+            if(array[absValue - 1] < 0) return value; //check if value at particular index is negative (x<0)
+            array[absValue - 1] *= -1; //lookup index, make value negative
+        }
+        return -1;
     }
 }
