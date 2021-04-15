daef4f66fc4d144119350e93cd320dc4e4e5ac54
arsalon <arsalonamini2020@gmail.com>
2021-04-15 17:28:26 +0800

    Construct a Binary Search Tree with insert, contains and remove methods - O(log(n)) time on average and O(1) space on average using an iterative approach, worst case O(n) time and O(1) space for a right / left skewed tree. Handle Edge cases for remove method.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Palindrome.isPalindrome("abcacba"));
+        var root = new BinarySearchTree.BST(10);
+
+        root.leftNode = new BinarySearchTree.BST(5);
+        root.leftNode.leftNode = new BinarySearchTree.BST(2);
+        root.leftNode.rightNode = new BinarySearchTree.BST(5);
+        root.leftNode.leftNode.leftNode = new BinarySearchTree.BST(1);
+
+        root.rightNode = new BinarySearchTree.BST(15);
+        root.rightNode.leftNode = new BinarySearchTree.BST(13);
+        root.rightNode.rightNode = new BinarySearchTree.BST(22);
+
+        root.rightNode.leftNode.leftNode = new BinarySearchTree.BST(12);
+        root.rightNode.leftNode.rightNode = new BinarySearchTree.BST(14);
+
+
+
+        root.insert(15);
+        root.contains(14);
+        root.remove(10);
+
+        System.out.println(root);
     }
 }
 
