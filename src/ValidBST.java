14b0c36bb20ba3ebc7c23e3844872a7fd3ae8f06
arsalon <arsalonamini2020@gmail.com>
2021-04-16 16:59:40 +0800

    isValidBST algorithm - given a binary search tree, validate that all nodes satisfy the binary search tree property (all right nodes are greater or equal than parent, all left nodes are less than parent node). in O(n) time and O(d) space were n is # of nodes in tree and d is the depth(length of longest branch).

---
+++ src/ValidBST.java
@@ -0,0 +1,25 @@
+public class ValidBST {
+    //O(n) time where n is the number of nodes in the tree 
+    //O(d) depth where d is the length of the longest branch
+    static class Node {
+        int value;
+        Node right;
+        Node left;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean isValidBST(Node tree){
+        return isValidBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean isValidBST(Node tree, int minValue, int maxValue){
+        if(tree == null) return true;
+        if(tree.value < minValue || tree.value > maxValue) return false;
+        if(tree.right !=null && !isValidBST(tree.right, tree.value, maxValue)) return false;
+        if(tree.left !=null && !isValidBST(tree.left, minValue, tree.value)) return false;
+        return true;
+    }
+}
