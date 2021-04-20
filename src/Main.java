5fba67b45e12623660018de4952fd05c3d1d37ec
arsalon <arsalonamini2020@gmail.com>
2021-04-20 17:40:04 +0800

    Construct Mininum Height  Binary Search - given an input array of sorted, distinct values, construct a minimum height binary search tree in optimial time of O(n) time, O(n) space where n is number of elements in input array.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        var root = new BinarySearchTree.BST(10);
-
-        root.leftNode = new BinarySearchTree.BST(5);
-        root.leftNode.leftNode = new BinarySearchTree.BST(2);
-        root.leftNode.rightNode = new BinarySearchTree.BST(5);
-        root.leftNode.leftNode.leftNode = new BinarySearchTree.BST(1);
-
-        root.rightNode = new BinarySearchTree.BST(15);
-        root.rightNode.leftNode = new BinarySearchTree.BST(13);
-        root.rightNode.rightNode = new BinarySearchTree.BST(22);
-
-        root.rightNode.leftNode.leftNode = new BinarySearchTree.BST(12);
-        root.rightNode.leftNode.rightNode = new BinarySearchTree.BST(14);
+        List<Integer> array = Arrays.asList(1, 2, 5, 7, 10, 13, 14, 15, 22);
 
 
+        ConstructMinHeightBST.minHeightBSTSoln2(array);
 
-        root.insert(15);
-        root.contains(14);
-        root.remove(10);
+        System.out.println(array);
 
-        System.out.println(root);
+
     }
 }
 
