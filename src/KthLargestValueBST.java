7e94bf633a65155aa8b5d81320c4a1c9d5cc990f
arsalon <arsalonamini2020@gmail.com>
2021-04-21 18:20:40 +0800

    Optimal solution - O(h + k) time where h is the height of the BST, O(h) space by performing a reverse inOrderTraverse and stopping after k nodes have been processed.

--- src/KthLargestValueBST.java
+++ src/KthLargestValueBST.java
@@ -12,9 +12,19 @@ public class KthLargestValueBST {
         }
     }
 
+    static class TreeInfo{
+        public int numberOfNodesVisited;
+        public int latestVisitedNodeValue;
+
+        public TreeInfo(int numberOfNodesVisited, int latestVisitedNodeValue) {
+            this.numberOfNodesVisited = numberOfNodesVisited;
+            this.latestVisitedNodeValue = latestVisitedNodeValue;
+        }
+    }
+
     //O(n) time, O(n) space where n is number of nodes in BST
     //performs an inOrderTraverse to get nodes in sorted order
-    //returns the kth largest element from the sorted output of inOrderTraversal 
+    //returns the kth largest element from the sorted output of inOrderTraversal
     public int findKthLargestValueInBST(Node root, int k){
         ArrayList<Integer> sortedNodeValues = new ArrayList<Integer>();
         inOrderTraverse(root, sortedNodeValues);
@@ -27,5 +37,24 @@ public class KthLargestValueBST {
         inOrderTraverse(node.left, sortedNodeValues);
         sortedNodeValues.add(node.value);
         inOrderTraverse(node.right, sortedNodeValues);
+    }
+
+    //O(h + k) time where h is the height of the BST
+    //O(h) space where h is height of BST on recursive stack
+    public int findKthLargestValueInBSTv2(Node node, int k){
+        var treeInfo = new TreeInfo(0, -1);
+        reverseInOrderTraverse(node,k, treeInfo);
+        return treeInfo.latestVisitedNodeValue;
+    }
+
+    public void reverseInOrderTraverse(Node node, int k , TreeInfo treeInfo){
+        if(node == null || treeInfo.numberOfNodesVisited >= k) return;
+
+        reverseInOrderTraverse(node.right, k, treeInfo);
+        if(treeInfo.numberOfNodesVisited < k){
+            treeInfo.numberOfNodesVisited += 1;
+            treeInfo.latestVisitedNodeValue = node.value;
+            reverseInOrderTraverse(node.left, k, treeInfo);
+        }
     }
 }
