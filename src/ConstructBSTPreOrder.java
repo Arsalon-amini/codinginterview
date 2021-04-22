dd4517d8df135cb00736d6162caa8444dfe9ba0d
arsalon <arsalonamini2020@gmail.com>
2021-04-22 17:40:48 +0800

    Construct a BST from an input array of values that map to a preOrderTraversal of a BST. Soln 2 - O(n) time vs. O(n^2) soln 1 and O(n) space.

--- src/ConstructBSTPreOrder.java
+++ src/ConstructBSTPreOrder.java
@@ -1,9 +1,6 @@
 import java.util.List;
 
 public class ConstructBSTPreOrder {
-    //takes an input array of values corresponding to PreOrderBST traversal
-    //write a fn that takes input and builds a BST
-    //O(n^2) time, O(n) space in output but O(h) space in creating the BST where h is height of tree
 
     static class Node {
         int value;
@@ -15,6 +12,9 @@ public class ConstructBSTPreOrder {
         }
     }
 
+    //takes an input array of values corresponding to PreOrderBST traversal
+    //write a fn that takes input and builds a BST
+    //O(n^2) time, O(n) space in output but O(h) space in creating the BST where h is height of tree
     public static Node constructBST(List<Integer> preOrderTraversalValues){
         if(preOrderTraversalValues.size() == 0) return null;
 
@@ -43,5 +43,35 @@ public class ConstructBSTPreOrder {
         return node;
     }
 
+    static class TreeInfo {
+        public int rootIdx;
+
+        public TreeInfo(int rootIdx) {
+            this.rootIdx = rootIdx;
+        }
+    }
+
+    //O(n) time, O(n) space
+    public Node constructBSTv2(List<Integer> preOrderTraversalValues){
+        var treeInfo = new TreeInfo(0);
+        return constructBSTFromRange(Integer.MIN_VALUE, Integer.MAX_VALUE, preOrderTraversalValues, treeInfo);
+    }
+
+    public Node constructBSTFromRange(int lowerBound, int upperBound, List<Integer> preOrderTraversalValues, TreeInfo currentSubTreeInfo){
+        if(currentSubTreeInfo.rootIdx == preOrderTraversalValues.size()) return  null;
+
+        var rootValue = preOrderTraversalValues.get(currentSubTreeInfo.rootIdx);
+        if(rootValue < lowerBound || rootValue >= upperBound) return null; //check if valid node
+
+        currentSubTreeInfo.rootIdx += 1; //valid subtree root confirmed
+
+        Node leftSubTree = constructBSTFromRange(lowerBound, rootValue, preOrderTraversalValues, currentSubTreeInfo);
+        Node rightSubTree = constructBSTFromRange(rootValue, upperBound, preOrderTraversalValues, currentSubTreeInfo);
+
+        Node node = new Node(rootValue);
+        node.left = leftSubTree;
+        node.right = rightSubTree;
+        return node;
+    }
 
 }
