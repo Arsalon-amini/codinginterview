94a3cc816aa85ad45ab673b2d61dbf9697c8bf70
arsalon <arsalonamini2020@gmail.com>
2021-04-26 14:57:32 +0800

    FindSuccessor optimized solution - O(h) time, where h is height of tree, O(1) space | O(n) time, O(1) space worst case - uses two helper methods to determine succesor through understanding inOrder traversal. if target has rightChild, successor is furtherst left node in rightSubtree, if target successor is root of leftSubtree containing the target node.

--- src/FindSuccessor.java
+++ src/FindSuccessor.java
@@ -22,6 +22,33 @@ public class FindSuccessor {
         output.add(root);
         if(root.right != null) traverseInOrder(root.right, output);
     }
+
+
+    //Solution 2 - (Optimized)
+    //Complexity analysis - O(h) time, O(1) space | O(n) time, O(1) space worst case
+    //where h is the height of the tree (longest branch)
+    //target hasRightSubtree -> successor = leftMostValue in rightSubtree of target (left, visit, right)
+    //target !hasRightSubTree -> successor = first parent where target is in parent's leftSubtree
+    public static Node findSuccessorV2(Node root, Node target){
+        if(target.right != null) return getLeftMostChild(target.right);
+        return getRightMostParent(target);
+    }
+
+    public static Node getLeftMostChild(Node node){
+        var current = node;
+        while(current.left != null){
+            current = current.left;
+        }
+        return current; //leftMost leaf node
+    }
+
+    public static Node getRightMostParent(Node node){
+        var current = node;
+        while(current.parent != null && current.parent.right == current){
+            current = current.parent;
+        }
+        return current.parent; //have reached root of leftSubTree (left, this, right)
+    }
     static class Node{
         public int value;
         public Node right = null;
