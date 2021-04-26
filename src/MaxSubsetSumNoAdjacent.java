31eff9c8535a6e7dc5d03e9eac4a36831d4060e5
arsalon <arsalonamini2020@gmail.com>
2021-04-26 22:53:35 +0800

    Soln 2 - in O(n) time, O(1) space where n is the length of th einput array

--- src/MaxSubsetSumNoAdjacent.java
+++ src/MaxSubsetSumNoAdjacent.java
@@ -12,4 +12,20 @@ public class MaxSubsetSumNoAdjacent {
         }
         return maxSums[array.length - 1]; //last element in maxSums array is maxSubSetNoAdjacent
     }
+
+    //O(n) time where n is length of the array
+    //O(1) space - only storing two values at a time in memory
+    public static int maxSubSetSumNoAdjacentV2(int [] array){
+        if(array.length == 0) return 0;
+        if(array.length == 1) return array[0];
+
+        int second = array[0];
+        int first = Math.max(array[0], array[1]);
+        for(int i = 2; i < array.length; i++){
+            int current = Math.max(first, second + array[i]);
+            second = first;
+            first = current;
+        }
+        return first; //current = first = max(first, second + array[lastIdx])
+    }
 }
