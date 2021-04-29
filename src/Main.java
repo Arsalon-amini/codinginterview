0c8a37483aca3f83ec611a94cc45e4470d519ec9
arsalon <arsalonamini2020@gmail.com>
2021-04-29 22:35:48 +0800

    NumberOfWaysToTraverseGraph - dynamic programming approach - in O(NM) time | O(NM) space where N is length of width and M is length of height of graph. NOTE = can optimize space by only storing partial values of 2D intermediary array.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        int width = 4;
+        int height = 3;
+        NumberOfWaysToTraverseGraph.numberOfWaysToTraverseGraph(width, height);
     }
 }
 
--- src/NumberOfWaysToTraverseGraph.java
+++ src/NumberOfWaysToTraverseGraph.java
@@ -8,4 +8,26 @@ public class NumberOfWaysToTraverseGraph {
         return numberOfWaysToTraverseGraphRecursive(width - 1, height)
                 + numberOfWaysToTraverseGraphRecursive(width, height - 1);
     }
+
+    //Dynamic programming -
+    //O(NM) time | O(NM) space where N = width and M = height of graph
+    //space comes from making a 2D array for storing intermediary values
+    //can lower space complexity to O(Min(N,M)) by storing only portions of 2D array
+    public static int numberOfWaysToTraverseGraph(int width, int height){
+        int [][] numberOfWays = new int[height + 1][width + 1];
+
+        for(int columnIdx = 1; columnIdx < width + 1; columnIdx++){
+            for(int rowIdx = 1; rowIdx < height + 1; rowIdx++){
+                if(columnIdx == 1 || rowIdx == 1){
+                    numberOfWays[rowIdx][columnIdx] = 1; //base case
+                } else {
+                    int waysLeft = numberOfWays[rowIdx][columnIdx - 1]; //look left
+                    int waysUp = numberOfWays[rowIdx - 1][columnIdx]; //look up
+                    numberOfWays[rowIdx][columnIdx] = waysLeft + waysUp;
+                }
+            }
+        }
+        return numberOfWays[height][width];
+    }
+
 }
