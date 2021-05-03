13f5fc926b32f949283f8b4e12a658c9c9ef5936
arsalon <arsalonamini2020@gmail.com>
2021-05-03 16:32:22 +0800

    BreadthFirstSearch algorithm - given an acyclic graph like structure that contains Nodes who each have a name and an optional array of children, traverse in breadthfirst fashion - O(v + e) time where v is number of vertices (edges) and e is the number of edges. O(v) time.

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BreadthFirstSearch {
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<Node>();
+
+        public Node(String name) {
+           this.name = name;
+        }
+        public Node addChild(String name){
+            var newNode = new Node(name);
+            children.add(newNode);
+            return this;
+        }
+
+        //O(v + e) time | O(v) space
+        public List<String> breadthFirstSearch(List<String> array){
+            Queue<Node> queue = new LinkedList<Node>();
+            queue.add(this);
+            
+            while(!queue.isEmpty()) {
+                var current = queue.poll(); //pops out element at top of queue
+                array.add(current.name);
+                queue.addAll(current.children);
+            }
+            return array;
+        }
+    }
+}
