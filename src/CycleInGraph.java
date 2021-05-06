bd2784a2ad96780f38c6029f80ad3e19562fe158
arsalon <arsalonamini2020@gmail.com>
2021-05-06 16:58:43 +0800

    CycleInGraph Algorithm - O(V+E) time, O(V) space where V is # vvertices and E is # of edges, instead of using two auxiliary array's for visited and inStack, use only one color array and store information in unvisited = 0, inStack = 1, completed = 2 (white, grey, black).

--- src/CycleInGraph.java
+++ src/CycleInGraph.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CycleInGraph {
@@ -33,6 +34,45 @@ public class CycleInGraph {
         }
         inStack[vertex] = false; //remove value from inStack after visiting all edges
         return false;
+    }
+
+    //-------Solution2---------
+    //O(V + E) time, where V is # vertices and E is number of edges
+    //O(V) space - creating auxiliary "colors" array to store information
+    //unVisited = 0, inStack = 1, completed = 2
+    public int WHITE = 0; //unvisited
+    public int GREY = 1; //inStack
+    public int BLACK = 2; //completed
+    public boolean hasCycleV2(int[][]edges){
+        int numVertices = edges.length;
+        int[] colors = new int[numVertices];
+        Arrays.fill(colors, WHITE);
+
+        for(int vertex = 0; vertex < colors.length; vertex++){
+            if(colors[vertex] != WHITE) continue;
+            boolean containsCycle = depthFirstColoring(vertex, edges, colors);
+            if(containsCycle) return true;
+        }
+        return false;
+    }
+
+    public boolean depthFirstColoring(int vertex, int[][]edges, int[] colors){
+        colors[vertex] = GREY; //update current vertex to inStack
+
+        int [] neighbors = edges[vertex];
+        for(int neighbor : neighbors){
+            int neighborColor = colors[neighbor];
+            if(neighborColor == GREY)
+                return true;
+            if(neighborColor == BLACK)
+                continue;
+
+            //optimization to terminate recursive early after first cycle detected
+            boolean hasCycle = depthFirstColoring(neighbor, edges, colors);
+            if(hasCycle) return true;
+        }
+        colors[vertex] = BLACK; //traversed through all edges of current node, completed DFS for this node (moving up call stack)
+        return false; //if cycle is present, will return in the for each loop
     }
 
 }
