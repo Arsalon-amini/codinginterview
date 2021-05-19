d93a4c68f216f1b5d8952650c6707370d798ea14
arsalon <arsalonamini2020@gmail.com>
2021-05-19 14:10:56 +0800

    StairCaseTraversal - Soln 2 - recursive approach using memoization to bring complexity to O(n*k) time O(n) space where n = height and k = maxSteps.

--- src/StairCaseTraversal.java
+++ src/StairCaseTraversal.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
+
 public class StairCaseTraversal {
+    ///O(k^n) time | O(n) space - where k is maxSteps and n is height
     public static int staircaseTraversal(int height, int maxSteps){
         return getNumberOfWaysToTop(height, maxSteps);
     }
@@ -12,5 +15,25 @@ public class StairCaseTraversal {
             numberOfWays += getNumberOfWaysToTop(height - step, maxSteps);
         }
         return numberOfWays;
+    }
+
+    //O(n * k) time | O(n) space - where n is height and k is maxSteps
+    public static int stairCaseTraversalV2(int height, int maxSteps){
+        HashMap<Integer, Integer> memoize = new HashMap<Integer, Integer>();
+        memoize.put(0, 1);
+        memoize.put(1, 1);
+        return getNumberOfWaysToTopV2(height, maxSteps, memoize);
+    }
+
+    public static int getNumberOfWaysToTopV2(int height, int maxSteps, HashMap<Integer, Integer> memoize){
+       if(memoize.containsKey(height))
+           return memoize.get(height);
+
+        int numWaysToTop = 0;
+        for(int step = 1; step < Math.min(height, maxSteps) + 1; step++){
+            numWaysToTop += getNumberOfWaysToTopV2(height - step, maxSteps, memoize);
+        }
+        memoize.put(height, numWaysToTop);
+        return numWaysToTop;
     }
 }
