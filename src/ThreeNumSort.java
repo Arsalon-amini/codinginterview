f7f67fa984f5304d0a1f8e11c8e21ff953345689
arsalon <arsalonamini2020@gmail.com>
2021-05-21 20:53:55 +0800

    Three Number Sorting Algorithm - Soln 3 - using three pointers, O(n) time | O(1) space where n is the lenght of the input array, only requires a single O(n) pass across the array to sort.

--- src/ThreeNumSort.java
+++ src/ThreeNumSort.java
@@ -66,9 +66,38 @@ public class ThreeNumSort {
         return array;
     }
 
+
+    public int [] threeNumberSortV3(int [] array, int [] order){
+        int firstValue = order[0];
+        int secondValue = order[1];
+
+        int firstIdx = 0;
+        int secondIdx = 0;
+        int thirdIdx = array.length - 1;
+
+        while(secondIdx <= thirdIdx){
+            int value = array[secondIdx];
+
+            if(value == firstValue){
+                swap(array, firstIdx, secondIdx);
+                firstIdx++;
+                secondIdx++;
+            } else if (value == secondValue){
+                secondIdx++;
+            } else {
+                swap(array, secondIdx, thirdIdx);
+                thirdIdx--;
+            }
+        }
+
+
+        return array;
+}
+
     public void swap (int [] array, int i, int j){
         int temp = array[j];
         array[j] = array[i];
         array[i] = temp;
     }
+
 }
